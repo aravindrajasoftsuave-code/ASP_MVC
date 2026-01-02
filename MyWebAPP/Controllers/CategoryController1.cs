10f192c397e4f3852bde0aae67224f52fe23a37d
Admin <Admin@SOFTSUAVE>
2026-01-02 20:03:47 +0530

    Add Category

---
+++ MyWebAPP/Controllers/CategoryController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPP.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyWebAPP/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebAPP.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category_ID { get; set; }
+       
+        [Required]
+        public string Category_Name { get; set; }
+
+        public string Display_Order { get; set; }
+    }
+}
